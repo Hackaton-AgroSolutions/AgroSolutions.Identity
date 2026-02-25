bb7073fdc63641b01954d81dd339547dfaf5bd35
MarioGuilherme <marioguilhermedev@gmail.com>
2026-02-24 22:09:04 -0300

    fix: nome do serviço

--- AgroSolutions.Identity.API/Program.cs
+++ AgroSolutions.Identity.API/Program.cs
@@ -91,7 +91,7 @@ otel.ConfigureResource(resource =>
 });
 
 otel.WithMetrics(metrics => metrics
-    .AddOtlpExporter(otlpOptions => otlpOptions.Endpoint = new Uri(builder.Configuration["Jeager:Url"]!))
+    .AddOtlpExporter(otlpOptions => otlpOptions.Endpoint = new Uri(builder.Configuration["Jaeger:Url"]!))
     .AddAspNetCoreInstrumentation()
     .AddMeter(APP_NAME)
     .AddMeter("Microsoft.AspNetCore.Hosting")
@@ -101,7 +101,7 @@ otel.WithTracing(tracing =>
 {
     tracing.AddAspNetCoreInstrumentation();
     tracing.AddSource(APP_NAME);
-    tracing.AddOtlpExporter(otlpOptions => otlpOptions.Endpoint = new Uri(builder.Configuration["Jeager:Url"]!));
+    tracing.AddOtlpExporter(otlpOptions => otlpOptions.Endpoint = new Uri(builder.Configuration["Jaeger:Url"]!));
 });
 
 WebApplication app = builder.Build();
